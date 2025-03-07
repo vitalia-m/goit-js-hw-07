97b62b8ab7175ed50a46c1e389299ee9f9b4a2f0
Vitalia <vitaliya.m05@gmail.com>
2025-03-07 11:16:14 +0100

    3x tasks are done

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+const categoriesList = document.querySelector(`#categories`);
+
+const categoriesItems = categoriesList.querySelectorAll(`.item`);
+
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+categoriesItems.forEach((item) => {
+  const categoryTitle = item.querySelector("h2").textContent;
+
+  const categoryElementsCount = item.querySelectorAll("ul li").length;
+
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${categoryElementsCount}`);
+});
